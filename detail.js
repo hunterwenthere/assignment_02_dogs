477221cc3d0b925bab5da8628fae77efedaaa549
hunterwenthere <hunter.wentland35@gmail.com>
2023-02-12 17:01:11 -0500

    trying again

--- detail.js
+++ detail.js
@@ -4,6 +4,7 @@ function LoadDetailPage(petNum){
     const myData = JSON.parse(data);
     var showMyContent = document.getElementById("detailContent");
     var index = petNum - 1
+    
 
     showMyContent.innerHTML += `
     <div id="content" class="col">
